c05aca3a5fb510f579ff70b29d81548a5063539c
Caroline A Bandeira <carol_bandeira7@hotmail.com>
2023-08-28 20:42:24 -0300

    ajustments

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         domains: ['source.unsplash.com', 'quotes.rest', 'quote.rest'],
     },    
-    /* async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    } */
 
 }
 
